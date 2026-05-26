99c65f49182ea9d68f90def7e51091c06591fb2b
WhellKnight1327 <a22310311@ceti.mx>
2026-05-25 23:06:30 -0600

    Cambio local a base de atlas

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ console.log("URI:", process.env.MONGO_URI);
 const uri = process.env.MONGO_URI; //obtiene uri de .env
 
 const app = express(); //instancia de aplicación HTTP para poder empezar a consultar queries GET o POST
-const client = new MongoClient('mongodb://localhost:27017'); //usa conexion de mondogdb local
-//const client = new MongoClient(uri); //conexion base en la nube
+//const client = new MongoClient('mongodb://localhost:27017'); //usa conexion de mondogdb local
+const client = new MongoClient(uri); //conexion base en la nube
 
 client.connect() //se tiene que conectar a la base de datos
     .then(() => console.log('Conexión completada a MongoDB')) //conexio completada
